0d81d964d90f72bfe675e21d8d148131f980ba4f
DataKumpel <tmbenkenstein@gmail.com>
2025-05-28 00:24:24 +0200

    Refactor: made creation of state struct smaller

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use camera::CameraUniform;
 use model::Model;
 use model::ModelUniform;
 use model::Texture;
+use wgpu::util::DeviceExt;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
@@ -13,6 +14,7 @@ use winit::window::{Window, WindowId};
 use std::sync::Arc;
 
 
+///// DEPTH BUFFER CREATION PROCEDURE //////////////////////////////////////////////////////////////
 fn create_depth_texture(device: &wgpu::Device, config: &wgpu::SurfaceConfiguration) -> Texture {
     let size = wgpu::Extent3d {
         width: config.width,
@@ -50,55 +52,40 @@ fn create_depth_texture(device: &wgpu::Device, config: &wgpu::SurfaceConfigurati
 
     Texture { texture, view, sampler }
 }
+///// DEPTH BUFFER CREATION PROCEDURE //////////////////////////////////////////////////////////////
 
-
-///// STATE STRUCTURE //////////////////////////////////////////////////////////////////////////////
-struct State {
+///// GPU STRUCTURE ////////////////////////////////////////////////////////////////////////////////
+struct GPU {
+    instance: wgpu::Instance,
     surface: wgpu::Surface<'static>,
+    adapter: wgpu::Adapter,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
-    size: winit::dpi::PhysicalSize<u32>,
-    render_pipeline: wgpu::RenderPipeline,
-
-    // Camera:
-    camera: Camera,
-    camera_uniform: CameraUniform,
-    camera_buffer: wgpu::Buffer,
-    camera_bind_group: wgpu::BindGroup,
-
-    // Model:
-    model: Option<Model>,
-    model_uniform: ModelUniform,
-    model_buffer: wgpu::Buffer,
-    model_bind_group: wgpu::BindGroup,
-
-    // Depth-buffer:
-    depth_texture: Texture
 }
 
-impl State {
-    async fn new(window: &Arc<Window>) -> Self {
-        let size = window.inner_size();
+impl GPU {
+    pub async fn new(window: &Arc<Window>, size: winit::dpi::PhysicalSize<u32>) -> Self {
         let instance = wgpu::Instance::default();
+        
         let surface = instance.create_surface(window.clone()).unwrap();
-
+        
         let adapter = instance.request_adapter(&wgpu::RequestAdapterOptionsBase { 
             power_preference: wgpu::PowerPreference::HighPerformance, 
             force_fallback_adapter: false, 
             compatible_surface: Some(&surface), 
         }).await.unwrap();
-
+    
         let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
             required_features: wgpu::Features::empty(),
             required_limits: wgpu::Limits::default(),
             memory_hints: wgpu::MemoryHints::default(),
             label: None,
         }, None).await.unwrap();
-
+    
         let surface_caps = surface.get_capabilities(&adapter);
         let surface_format = surface_caps.formats[0];
-
+    
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface_format,
@@ -111,22 +98,40 @@ impl State {
         };
         surface.configure(&device, &config);
 
-        // ---> Load shaders:
-        let shader = device.create_shader_module(
+        Self{ instance, surface, adapter, device, queue, config }
+    }
+
+    pub fn load_shaders(&self) -> wgpu::ShaderModule{
+        // TODO: load multiple shaders... later ;)
+        self.device.create_shader_module(
             wgpu::ShaderModuleDescriptor { 
                 label: Some("Shader"), 
                 source: wgpu::ShaderSource::Wgsl(
                     std::fs::read_to_string("./src/shader.wgsl").unwrap().into(), 
                 ),
             }
-        );
+        )
+    }
+}
+///// GPU STRUCTURE ////////////////////////////////////////////////////////////////////////////////
 
-        // ---> Create Camera:
+///// CAMERA STATE STRUCTURE ///////////////////////////////////////////////////////////////////////
+struct CameraState {
+    camera: Camera,
+    camera_uniform: CameraUniform,
+    camera_buffer: wgpu::Buffer,
+    camera_bind_group_layout: wgpu::BindGroupLayout,
+    camera_bind_group: wgpu::BindGroup,
+}
+
+impl CameraState {
+    pub fn new(gpu: &GPU) -> Self {
+        let device = &gpu.device;
         let camera = Camera {
             eye: nalgebra_glm::vec3(0.0, 5.0, -10.0),
             target: nalgebra_glm::vec3(0.0, 0.0, 0.0),
             up: nalgebra_glm::vec3(0.0, 1.0, 0.0),
-            aspect: config.width as f32 / config.height as f32,
+            aspect: gpu.config.width as f32 / gpu.config.height as f32,
             fovy: 45.0_f32.to_radians(),
             z_near: 0.1,
             z_far: 100.0,
@@ -174,7 +179,24 @@ impl State {
             },
         );
 
-        // ---> Create ModelUniform:
+        Self { camera, camera_uniform, camera_buffer, camera_bind_group_layout, camera_bind_group }
+    }
+}
+///// CAMERA STATE STRUCTURE ///////////////////////////////////////////////////////////////////////
+
+///// MODEL UNIFORM STATE STRUCTURE ////////////////////////////////////////////////////////////////
+struct ModelUniformState {
+    model: Option<Model>,
+    model_uniform: ModelUniform,
+    model_buffer: wgpu::Buffer,
+    model_bind_group_layout: wgpu::BindGroupLayout,
+    model_bind_group: wgpu::BindGroup,
+}
+
+impl ModelUniformState {
+    pub fn new(gpu: &GPU) -> Self {
+        let device = &gpu.device;
+        
         let model_uniform = ModelUniform::new();
 
         let model_buffer = device.create_buffer_init(
@@ -216,8 +238,32 @@ impl State {
             },
         );
 
-        // ---> Create material bind group:
-        let material_bind_group_layout = device.create_bind_group_layout(
+        Self { model: None, model_uniform, model_buffer, model_bind_group_layout, model_bind_group }
+    }
+}
+///// MODEL UNIFORM STATE STRUCTURE ////////////////////////////////////////////////////////////////
+
+///// STATE STRUCTURE //////////////////////////////////////////////////////////////////////////////
+struct State {
+    gpu: GPU,
+    size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
+
+    // Camera:
+    camera_state: CameraState,
+
+    // Model:
+    model_uniform_state: ModelUniformState,
+
+    // Depth-buffer:
+    depth_texture: Texture
+}
+
+impl State {
+    fn create_material_bind_group(gpu: &GPU) -> wgpu::BindGroupLayout {
+        let device = &gpu.device;
+
+        device.create_bind_group_layout(
             &wgpu::BindGroupLayoutDescriptor {
                 label: Some("Material bind group layout"),
                 entries: &[
@@ -263,26 +309,32 @@ impl State {
                     },
                 ],
             },
-        );
+        )
+    }
 
+    fn create_render_pipeline(gpu: &GPU, 
+                              camera_bgl: &wgpu::BindGroupLayout, 
+                              model_bgl: &wgpu::BindGroupLayout, 
+                              material_bgl: &wgpu::BindGroupLayout,
+                              shader: wgpu::ShaderModule) -> wgpu::RenderPipeline{
+        let device = &gpu.device;
 
-        // ---> Create Depth Texture:
-        let depth_texture = create_depth_texture(&device, &config);
+        let surface_caps = gpu.surface.get_capabilities(&gpu.adapter);
+        let surface_format = surface_caps.formats[0];
 
-        // ---> Create pipeline:
         let render_pipeline_layout = device.create_pipeline_layout(
             &wgpu::PipelineLayoutDescriptor { 
                 label: Some("Render Pipeline Layout"), 
                 bind_group_layouts: &[
-                    &camera_bind_group_layout,    // @group(0)
-                    &model_bind_group_layout,     // @group(1)
-                    &material_bind_group_layout,  // @group(2)
+                    &camera_bgl,    // @group(0)
+                    &model_bgl,     // @group(1)
+                    &material_bgl,  // @group(2)
                 ], 
                 push_constant_ranges: &[],
             },
         );
 
-        let render_pipeline = device.create_render_pipeline(
+        device.create_render_pipeline(
             &wgpu::RenderPipelineDescriptor { 
                 label: Some("Render Pipeline"), 
                 layout: Some(&render_pipeline_layout), 
@@ -322,55 +374,78 @@ impl State {
                 multiview: None, 
                 cache: None,
              },
+        )
+    }
+
+    async fn new(window: &Arc<Window>) -> Self {
+        let size = window.inner_size();
+        
+        // ---> Initialize GPU:
+        let gpu = GPU::new(window, size).await;
+
+        // ---> Load shaders:
+        let shader = gpu.load_shaders();
+
+        // ---> Create Camera:
+        let camera_state = CameraState::new(&gpu);
+
+        // ---> Create ModelUniform:
+        let mut model_uniform_state = ModelUniformState::new(&gpu);
+
+        // ---> Create material bind group:
+        let material_bind_group_layout = Self::create_material_bind_group(&gpu);
+
+        // ---> Create Depth Texture:
+        let depth_texture = create_depth_texture(&gpu.device, &gpu.config);
+
+        // ---> Create pipeline:
+        let render_pipeline = Self::create_render_pipeline(
+            &gpu, &camera_state.camera_bind_group_layout, &model_uniform_state.model_bind_group_layout, 
+            &material_bind_group_layout, shader,
         );
 
         // ---> Load a Model (test):
-        let model = model::load_model("models/Bridge.glb", &device, &queue, 
-                                      &material_bind_group_layout).ok();
+        model_uniform_state.model = model::load_model("models/Bridge.glb", &gpu.device, &gpu.queue, 
+                                                      &material_bind_group_layout).ok();
         
-        Self {
-            surface, device, queue, config, size, render_pipeline,
-            camera, camera_uniform, camera_buffer, camera_bind_group,
-            model, model_uniform, model_buffer, model_bind_group,
-            depth_texture,
-        }
+        Self { gpu, size, render_pipeline, camera_state, model_uniform_state, depth_texture }
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         if new_size.width > 0 && new_size.height > 0 {
             self.size = new_size;
-            self.config.width = new_size.width;
-            self.config.height = new_size.height;
+            self.gpu.config.width = new_size.width;
+            self.gpu.config.height = new_size.height;
 
             // ---> Reconfigure surface:
-            self.surface.configure(&self.device, &self.config);
+            self.gpu.surface.configure(&self.gpu.device, &self.gpu.config);
 
             // ---> Recreate depth texture:
-            self.depth_texture = create_depth_texture(&self.device, &self.config);
+            self.depth_texture = create_depth_texture(&self.gpu.device, &self.gpu.config);
 
             // ---> Update camera aspect ratio:
-            self.camera.aspect = self.config.width as f32 / self.config.height as f32;
-            self.camera_uniform.update_view_proj(&self.camera);
+            self.camera_state.camera.aspect = self.gpu.config.width as f32 / self.gpu.config.height as f32;
+            self.camera_state.camera_uniform.update_view_proj(&self.camera_state.camera);
 
             // ---> Update camera uniform buffer:
-            self.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
+            self.gpu.queue.write_buffer(&self.camera_state.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_state.camera_uniform]));
         }
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         // ---> Get current image (FrameBuffer):
-        let output = match self.surface.get_current_texture() {
+        let output = match self.gpu.surface.get_current_texture() {
             Ok(frame) => frame,
             Err(_) => {
-                self.surface.configure(&self.device, &self.config);
-                self.surface.get_current_texture()
-                            .expect("Failed to acquire next swap chain texture!")
+                self.gpu.surface.configure(&self.gpu.device, &self.gpu.config);
+                self.gpu.surface.get_current_texture()
+                                .expect("Failed to acquire next swap chain texture!")
             }
         };
         let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
 
         // ---> Command encoder for GPU commands:
-        let mut encoder = self.device.create_command_encoder(
+        let mut encoder = self.gpu.device.create_command_encoder(
             &wgpu::CommandEncoderDescriptor { label: None }
         );
 
@@ -402,11 +477,11 @@ impl State {
             render_pass.set_pipeline(&self.render_pipeline);
 
             // ---> Set bind groups for camera and model:
-            render_pass.set_bind_group(0, &self.camera_bind_group, &[]); // Camera data
-            render_pass.set_bind_group(1, &self.model_bind_group, &[]);  // Model transformations
+            render_pass.set_bind_group(0, &self.camera_state.camera_bind_group, &[]);        // Camera data
+            render_pass.set_bind_group(1, &self.model_uniform_state.model_bind_group, &[]);  // Model transformations
 
             // ---> Render model (if exists...):
-            if let Some(model) = &self.model {
+            if let Some(model) = &self.model_uniform_state.model {
                 for mesh in &model.meshes {
                     render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
                     render_pass.set_index_buffer(mesh.index_buffer.slice(..), 
@@ -424,7 +499,7 @@ impl State {
         // ---> End of render pass...
 
         // ---> Send to GPU to render the image:
-        self.queue.submit(Some(encoder.finish()));
+        self.gpu.queue.submit(Some(encoder.finish()));
         output.present();
 
         Ok(())
